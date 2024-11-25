b9c7e12c489ad12e1babb5db04f72ff47034ae28
Manikanta9959 <sivachallano1@gmail.com>
2024-11-26 01:31:51 +0530

    age diestruibution

--- api/controllers/csvtojson/controller.js
+++ api/controllers/csvtojson/controller.js
@@ -2,6 +2,7 @@ import { parseCSV } from '../utils/csvParser.js';
 import { pool } from '../../../db_module/session.js';
 import config from '../../../config.js';
 
+// API endpoint to upload csv file and update database
 export async function uploadCSV(req, res) {
     try {
         const records = parseCSV(config.csvFilePath);
@@ -11,20 +12,24 @@ export async function uploadCSV(req, res) {
             const fullName = `${name.firstName} ${name.lastName}`;
 
             await pool.query(
-                `INSERT INTO public.users (name, age, address, additional_info,serial4)
+                `INSERT INTO public.users (name, age, address, additional_info, serial4)
                  VALUES ($1, $2, $3, $4, $5)`,
-                [fullName, age, JSON.stringify(address), JSON.stringify(additionalInfo), 1]
-            );            
+                [fullName, age, JSON.stringify(address), JSON.stringify(additionalInfo),1]
+            );
         }
+        const ageDistribution = await calculateAgeDistribution();
 
-        res.status(200).send('CSV data uploaded successfully!');
+        res.status(200).json({
+            message: 'CSV data uploaded successfully!',
+            ageDistribution: ageDistribution,
+        });
     } catch (err) {
         console.error(err);
         res.status(500).send('Error uploading CSV data');
     }
 }
 
-export async function ageDistribution(req, res) {
+async function calculateAgeDistribution() {
     try {
         const result = await pool.query('SELECT age FROM public.users');
         const ageGroups = { '<20': 0, '20-40': 0, '40-60': 0, '>60': 0 };
@@ -37,8 +42,27 @@ export async function ageDistribution(req, res) {
             else ageGroups['>60']++;
         });
 
-        console.log('Age Distribution:', ageGroups);
-        res.status(200).json(ageGroups);
+        const totalUsers = result.rows.length;
+
+        const ageDistribution = {
+            '<20': ((ageGroups['<20'] / totalUsers) * 100).toFixed(2),
+            '20-40': ((ageGroups['20-40'] / totalUsers) * 100).toFixed(2),
+            '40-60': ((ageGroups['40-60'] / totalUsers) * 100).toFixed(2),
+            '>60': ((ageGroups['>60'] / totalUsers) * 100).toFixed(2)
+        };
+
+        return ageDistribution;
+    } catch (err) {
+        console.error('Error calculating age distribution:', err);
+        throw new Error('Error calculating age distribution');
+    }
+}
+
+// API endpoint to calculate and return age distribution
+export async function ageDistribution(req, res) {
+    try {
+        const ageDistribution = await calculateAgeDistribution();
+        res.status(200).json(ageDistribution);
     } catch (err) {
         console.error(err);
         res.status(500).send('Error calculating age distribution');
